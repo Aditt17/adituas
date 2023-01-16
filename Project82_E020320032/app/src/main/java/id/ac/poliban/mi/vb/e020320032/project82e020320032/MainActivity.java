c3a7070fe346adf99bdc948d1c766cbafa4be023
Aditt17 <116248532+Aditt17@users.noreply.github.com>
2023-01-16 14:38:01 +0700

    Project82_E020320032

---
+++ Project82_E020320032/app/src/main/java/id/ac/poliban/mi/vb/e020320032/project82e020320032/MainActivity.java
@@ -0,0 +1,15 @@
+package id.ac.poliban.mi.vb.e020320032.project82e020320032;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        setTitle("Adit - Project 82 E020320032");
+    }
+}
